400b934d357e7a5d09d02c76f826b7f78f0a0107
Loris Fasolo <loris@revodigital.it>
2024-11-04 15:45:57 +0100

    .

---
+++ scriptone.js
@@ -0,0 +1,13 @@
+function mapRenderLoris(accessToken, mapControlId, options) {
+    const map = new mapboxgl.Map({
+        container: mapControlId,
+        center: options.defaultView,
+        zoom: 13,
+        style: 'mapbox://styles/mapbox/standard',
+        config: {
+            basemap: {
+                lightPreset: 'night'
+            }
+        }
+    });
+}
